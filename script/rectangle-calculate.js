3e7b9550a7924979658e043e09b33d15862349cc
Sabbish99 <programmersabbir99@gmail.com>
2024-08-20 21:26:18 +0600

    second

--- script/rectangle-calculate.js
+++ script/rectangle-calculate.js
@@ -1,4 +1,25 @@
 
 function calculateRectangleArea() {
-    console.log('hala madrid');
+
+    // get length
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
+    console.log(length);
+
+    // get width 
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+
+    // calculate ractangle area
+    const area = length * width;
+    console.log(area);
+
+
+    // display rectangle area
+    const rectangleAreaSpan = document.getElementById('rectangle-area');
+    rectangleAreaSpan.innerText = area;
 }
